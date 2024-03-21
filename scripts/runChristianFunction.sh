0d4c1d252d12197fba12c8cb8af3c01989bb674d
Christian Coffman <coffm049@umn.edu>
2024-03-21 12:30:15 -0500

    Finished tutorial

--- scripts/runChristianFunction.sh
+++ scripts/runChristianFunction.sh
@@ -1,11 +1,3 @@
 module load R
 
 Rscript scripts/ChristianFunction.R
-
-# make a for loop that prints the alphabet backwards
-
-# Path: scripts/printAlphabet.sh
-for letter in z y x w v u t s r q p o n m l k j i h g f e d c b a
-do
-  echo $letter
-done
